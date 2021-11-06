be91e86e24e9ab208c6632a3922a1cd0b7b9d74f
Dennis Law <denn15_law@live.ca>
2021-11-06 05:50:47 +0000

    refractor to add Transaction class

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 let balance = 500.0;
 
-class Deposit {
+class Transaction {
   constructor(amount, account) {
     this.amount = amount;
-
-    //pass in Account class into here
     this.account = account;
   }
+}
 
+class Deposit extends Transaction {
   commit() {
     this.account.balance += this.amount;
   }
 }
 
-class Withdrawal {
-  constructor(amount, account) {
-    this.amount = amount;
-    this.account = account;
-  }
-
+class Withdrawal extends Transaction {
   commit() {
     this.account.balance -= this.amount;
   }
